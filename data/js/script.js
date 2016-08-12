87d7c100014b93cc1414ea0db4ded54bbe93ed57
Sukhbir Singh <sukhbirbedi03@gmail.com>
2016-08-12 18:59:24 +0530

    Update script.js

--- data/js/script.js
+++ data/js/script.js
@@ -1,25 +1,32 @@
+//#e67e22-Orange Shade
+//#3498db-Blue Shade
 
 
+//Handling animations on mouseover;
 $(".box").on("mouseover", function(){
 	var x = $(this).attr("class");
 	if(x == "box clicked"){
+		// if box is clicked retain orange shade
 		$(this).css("backgroundColor", "#e67e22");
 	}else{
+		// if box isnt clicked change to blue
 		$(this).css("backgroundColor", "#3498db");
 	}
 });
 
-
+//Handling Animations on Mouseout
 $(".box").on("mouseout", function(){
 	var x = $(this).attr("class");
 	if(x == "box clicked"){
+		// if box is clicked retain orange shade;
 		$(this).css("backgroundColor", "#e67e22");
 	}else{
+		// if box isnt clicked change back to theme color;
 		$(this).css("backgroundColor",$("#colorDisplay").text());
 	}
 });
 
-
+//Handling Animations on User Click
 $(".box").on("click", function(){
 	var z =$(this).css("backgroundColor")
 	if(z == "rgb(52, 152, 219)"){
@@ -32,6 +39,7 @@ $(".box").on("click", function(){
 	}
 });
 
+//Clearing the color pencil and display hidden content
 $("#delete").on("click", function(){
 	$(".contain, #colorControl").fadeOut(700,function(){
 		$(this).remove();
@@ -42,16 +50,18 @@ $("#delete").on("click", function(){
 	})
 });
 
+// Reset game
 $("#reset").on("click", function(){
 	$("div.box").removeClass("clicked");
 	$("div.box").css("backgroundColor", $("#colorDisplay").text());
 });
 	
-
+//go back to the game. from the hidden message page
 $(".reset1").on("click", function(){
    location.reload();
 })
 
+//input responsible for changing theme color and reseting the game;
 $("input").on("change", function(){
 	var color= $(this).val();
 	$("#colorDisplay").text(color);
